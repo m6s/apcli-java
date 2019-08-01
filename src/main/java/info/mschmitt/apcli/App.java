47f2d1bca0e81900d4f2c193769ae05769b438e6
Matthias Schmitt <m6s@mschmitt.info>
2019-08-01 23:34:07 +0200

    CLI

--- src/main/java/info/mschmitt/apcli/App.java
+++ src/main/java/info/mschmitt/apcli/App.java
@@ -1,7 +1,27 @@
 package info.mschmitt.apcli;
 
+import org.apache.commons.cli.*;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Options options = new Options();
+        options.addOption("t", false, "display current time");
+        options.addOption("help", false, "print this message");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine line = null;
+        try {
+            line = parser.parse(options, args);
+        } catch (ParseException exp) {
+            System.err.println("Parsing failed.  Reason: " + exp.getMessage());
+            return;
+        }
+        if (line.hasOption("help")) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("apcli", options);
+        } else if (line.hasOption("t")) {
+            System.out.println("Hello t");
+        } else {
+            System.out.println("Hello world!");
+        }
     }
 }
