a22b127aa0cf71518251c89c8f6ee8b598ae1445
Matthias Schmitt <m6s@mschmitt.info>
2019-08-01 23:40:56 +0200

    CLI 2

--- src/main/java/info/mschmitt/apcli/App.java
+++ src/main/java/info/mschmitt/apcli/App.java
@@ -3,22 +3,25 @@ package info.mschmitt.apcli;
 import org.apache.commons.cli.*;
 
 public class App {
+    private static final Option t = new Option("t", false, "display current time");
+    private static final Option help = new Option("help", false, "print this message");
+
     public static void main(String[] args) {
         Options options = new Options();
-        options.addOption("t", false, "display current time");
-        options.addOption("help", false, "print this message");
+        options.addOption(t);
+        options.addOption(help);
         CommandLineParser parser = new DefaultParser();
-        CommandLine line = null;
+        CommandLine line;
         try {
             line = parser.parse(options, args);
         } catch (ParseException exp) {
-            System.err.println("Parsing failed.  Reason: " + exp.getMessage());
+            System.err.println(exp.getMessage());
             return;
         }
-        if (line.hasOption("help")) {
+        if (line.hasOption(help.getOpt())) {
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp("apcli", options);
-        } else if (line.hasOption("t")) {
+        } else if (line.hasOption(t.getOpt())) {
             System.out.println("Hello t");
         } else {
             System.out.println("Hello world!");
